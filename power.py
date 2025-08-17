e6bf4884e0b6403d911284e5cc65df5d60ab217a
ganesh5002-code <hethaksh.y@gmail.com>
2025-08-17 13:48:01 +0800

    Initial commit

---
+++ power.py
@@ -0,0 +1,6 @@
+# making a power loop
+num = int(input("Enter a number greater than 1"))
+n = int(input("Enter a number greater than 1 for the exponent"))
+for y in range(1,n+1):
+    num = num*y
+print(num)
